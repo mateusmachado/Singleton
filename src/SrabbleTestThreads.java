4b8bd4a73e38561a0400bad0abd768275ea422e2
Mateus Machado <mateusmachado@wifi-joao-junior.ilegra.local>
2016-05-09 10:07:22 -0300

    Singleton

---
+++ src/SrabbleTestThreads.java
@@ -0,0 +1,9 @@
+public class SrabbleTestThreads {
+    public static void main(String[] args){
+        Runnable getTiles = new GetTheTiles();
+        Runnable getTilesAgain = new GetTheTiles();
+
+        new Thread(getTiles).start();
+        new Thread(getTilesAgain).start();
+    }
+}
